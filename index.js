9a5d1e89e722c63a517368beb462f881b9c11bed
davidhennig <davidjhennig@gmail.com>
2020-01-16 18:08:31 -0500

    port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require("./api/server.js");
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
   console.log("\n*** Server Running on http://localhost:5000 ***\n");
 });
